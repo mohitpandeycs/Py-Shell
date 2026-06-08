7c838d1487166b9bff25289951a67720900baf01
mohitpandeycs <191971259+mohitpandeycs@users.noreply.github.com>
2026-06-08 15:04:31 +0530

    Error Handling for Cmds

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-print("Mohit Pandey")
+import sys
+
+
+def main():
+    sys.stdout.write("$ ")
+    pass
+
+    command = input()
+    print(f"{command}: Command Not Found")
+
+if __name__ == "__main__":
+    main()
